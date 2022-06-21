ea88ce87525dc62e8c176d94ae9595862583a7c2
francescoautera <francesco.autera@gmail.com>
2022-06-21 12:36:42 +0200

    refactor + player change model every run

---
+++ GameJam/Assets/Scripts/PlayerChoiceModel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChoiceModel : MonoBehaviour {
+
+    public List<GameObject> playerModels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var model in playerModels) {
+            model.SetActive(false);
+        }
+        var index = Random.Range(0, playerModels.Count);
+        playerModels[index].SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
