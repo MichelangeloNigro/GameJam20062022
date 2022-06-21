cb4484c92b59a820d5e39144ad024d2e40d51882
Sara <sbolognesi95@gmail.com>
2022-06-21 11:57:32 +0200

    Added scriptable card

---
+++ GameJam/Assets/Scripts/Scriptable/AttackCard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCard : GeneralCard {
+    [SerializeField] private int damage;
+    //enemy
+    private void OnAttack( /*enemy*/) {
+	    //enemy.health -= damage;
+    }
+}
