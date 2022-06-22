8b8353fdbfdac0ce053461c138a610450c19fd8e
francescoautera <francesco.autera@gmail.com>
2022-06-22 18:17:30 +0200

    add toolTip

---
+++ GameJam/Assets/Scripts/ToolTipManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ToolTipManager : MonoBehaviour {
+	public Canvas canvas;
+	public GameObject toolTip;
+	public TMP_Text header;
+	public TMP_Text text;
+	public static ToolTipManager instace;
+
+	private void Start() {
+		canvas.gameObject.SetActive(false);
+	}
+
+	private void Awake() {
+		instace = this;
+	}
+
+	public void ShowToolTip(string headerText, string description) {
+		canvas.gameObject.SetActive(true);
+		toolTip.transform.position = Input.mousePosition;
+		header.text = headerText;
+		text.text = description;
+
+	}
+
+
+	public void OffToolTip() {
+		canvas.gameObject.SetActive(false);
+	}
+}
--- GameJam/Assets/Scripts/UiCardDrawer.cs
+++ GameJam/Assets/Scripts/UiCardDrawer.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UiCardDrawer : MonoBehaviour
-{
+public class UiCardDrawer : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler{
+
+   
    public GeneralCard card;
    public TMP_Text name;
    public TMP_Text description;
@@ -33,4 +36,16 @@ public class UiCardDrawer : MonoBehaviour
       name.text = card.cardName;
       description.text = card.description;
       image.sprite = card.image;   }
+   
+   public void OnPointerEnter(PointerEventData eventData)
+   {
+      
+      ToolTipManager.instace.ShowToolTip(card.cardName,card.description);
+   }
+   
+
+   public void OnPointerExit(PointerEventData eventData) {
+      
+      ToolTipManager.instace.OffToolTip();
+   }
 }
